7a5288dcab5f157be900a6ca748ac3f918839f7c
YQCforest <128026304+YQCforest@users.noreply.github.com>
2023-04-01 10:03:56 +0800

    Update athletePingPang.java

--- Athlete_abstract_interface/athletePingPang.java
+++ Athlete_abstract_interface/athletePingPang.java
@@ -5,7 +5,7 @@ public class athletePingPang extends athlete implements speakEnglish{
     public void move() {
         System.out.println("正在训练乓乓球");
     }
-
+    //为了实现接口，要么是抽象类，要么重写接口所有中的抽象方法
     @Override
     public void speakEng(String name) {
         System.out.println(name+"正在说英语");
