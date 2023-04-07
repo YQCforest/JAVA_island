6e1d4fdec0ebf0d7e17415b271408ae3af3fc3f6
YQCforest <128026304+YQCforest@users.noreply.github.com>
2023-04-07 14:24:32 +0800

    Add files via upload

---
+++ Teacher/administrativeTeacher.java
@@ -0,0 +1,8 @@
+package Teacher;
+
+public class administrativeTeacher extends people{
+    @Override
+    public void move() {
+        System.out.println("行政老师主要负责学校管理任务！");
+    }
+}
---
+++ Teacher/teachingTeacher.java
@@ -0,0 +1,8 @@
+package Teacher;
+
+public class teachingTeacher extends people{
+    @Override
+    public void move() {
+        System.out.println("教学老师主要负责教学任务！");
+    }
+}
